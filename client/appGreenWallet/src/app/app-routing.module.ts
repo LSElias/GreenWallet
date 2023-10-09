ad198098b8cf2468268363dc4b0c2fe5acff9090
LSElias <eliasrodguer12@gmail.com>
2023-10-08 18:45:35 -0600

    sc1 de 2 en home

--- client/appGreenWallet/src/app/app-routing.module.ts
+++ client/appGreenWallet/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './home/inicio/inicio.component';
+import { NoEncontradoComponent } from './core/no-encontrado/no-encontrado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'inicio',component: InicioComponent},
+  { path:'', redirectTo:'/inicio' , pathMatch:'full'},
+  { path:'**', component: NoEncontradoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/appGreenWallet/src/app/home/home.module.ts
+++ client/appGreenWallet/src/app/home/home.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-
+import { InicioComponent } from './inicio/inicio.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InicioComponent
+  ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule
   ]
 })
 export class HomeModule { }
