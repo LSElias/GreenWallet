a3a77f5670ae407dcf9bf2dc7fcc55115928bee0
LSElias <eliasrodguer12@gmail.com>
2023-10-20 22:12:31 -0600

    CatMat Img Bar

---
+++ client/appGreenWallet/src/app/materiales/diag-materiales/diag-materiales.component.ts
@@ -0,0 +1,42 @@
+import { Component, Inject,OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Subject, takeUntil } from 'rxjs';
+import { GenericService } from 'src/app/share/generic.service';
+
+@Component({
+  selector: 'app-diag-materiales',
+  templateUrl: './diag-materiales.component.html',
+  styleUrls: ['./diag-materiales.component.css']
+})
+export class DiagMaterialesComponent implements OnInit {
+  datos:any;
+  datosDialog:any;
+  destroy$:Subject<boolean>= new Subject<boolean>();
+  constructor(
+    @Inject(MAT_DIALOG_DATA) data,
+    private dialogRef:MatDialogRef<DiagMaterialesComponent>,
+    private gService:GenericService
+  ) { 
+    this.datosDialog=data;
+  }
+
+  ngOnInit(): void {
+    if(this.datosDialog.id){
+      this.obtenerMaterial(this.datosDialog.id);
+    }
+  }
+  obtenerMaterial(id:any){
+    this.gService
+    .get('datos/catmat/',id)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((data:any)=>{
+        this.datos=data; 
+    });
+   
+  }
+  close(){
+    //Dentro de close ()
+     //this.form.value 
+    this.dialogRef.close();
+  }
+}
--- client/appGreenWallet/src/app/materiales/materiales-routing.module.ts
+++ client/appGreenWallet/src/app/materiales/materiales-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexMaterialesComponent } from './index-materiales/index-materiales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'materiales',component: IndexMaterialesComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
