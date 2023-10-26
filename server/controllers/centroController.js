463b1a313a8c92969f4cdbe7c42d77f828f88a3f
Tylinn <tgonzalezb@est.utn.ac.cr>
2023-10-25 20:05:02 -0600

    Modificaciones

--- server/controllers/centroController.js
+++ server/controllers/centroController.js
@@ -26,27 +26,76 @@ module.exports.get = async (request, response, next) => {
 };
 
 //GetByIdCentro
-module.exports.getByIdUser = async (request, response, next) => {
-    let idUsuario = parseInt(request.params.idUsuario);
-    const billetera = await prisma.billetera.findUnique({
-      where: { idUsuario: idUsuario },
+module.exports.getByIdCentro = async (request, response, next) => {
+    let idCentro = parseInt(request.params.idCentro);
+    const centro = await prisma.centro.findUnique({
+      where: { idCentro: idCentro },
       include: {
-        usuario: true,
+        administrador: true,
+        direccion: true, 
+        horario: true,
+        materiales: {
+            select: {
+                nombre: true,
+                descripcion: true,
+                imagen: true,
+                color: true,
+                valor: true
+            }
+        }
       },
     });
-  
-    const datospers = {
-      usuario: {
-        cedula: billetera.usuario.cedula,
-        telefono: billetera.usuario.telefono,
-        correo: billetera.usuario.correo,
-        nombre: 
-        billetera.usuario.nombre + " " + billetera.usuario.apellido1 + " " + billetera.usuario.apellido2,
-      },
-      disponibles: billetera.disponibles,
-      canjeados: billetera.canjeadas,
-      total: billetera.total,
-    };
-  
-    response.json(datospers);
-  };
+  const datos= {
+    nombre: centro.nombre,
+    telefono: centro.telefono,
+    horario: centro.horario.dias + " - " + centro.horario.horas,
+    administrador: {
+        nombre: centro.administrador.nombre + " " + centro.administrador.apellido1 + " " + centro.administrador.apellido2,
+        correo: centro.administrador.correo,
+        cedula: centro.administrador.cedula
+    },
+    direccion: centro.direccion.provincia + ", " + centro.direccion.canton + ", " + 
+            centro.direccion.distrito + ". " + centro.direccion.senas,
+    materiales: centro.materiales 
+
+  }; 
+    response.json(datos);
+};
+
+//GetByIdUsuario
+module.exports.getByIdUser = async (request, response, next) => {
+    let idAdmin = parseInt(request.params.idAdmin);
+    const centro = await prisma.centro.findMany({
+      where: { idAdmin: idAdmin },
+      include: {
+        administrador: true,
+        direccion: true, 
+        horario: true,
+        materiales: {
+            select: {
+                nombre: true,
+                descripcion: true,
+                imagen: true,
+                color: true,
+                valor: true
+            }
+        }
+      }
+    });
+  const datos= centro.map(centro =>({
+    nombre: centro.nombre,
+    telefono: centro.telefono,
+    horario: centro.horario.dias + " - " + centro.horario.horas,
+    administrador: {
+        nombre: centro.administrador.nombre + " " + centro.administrador.apellido1 + " " + centro.administrador.apellido2,
+        correo: centro.administrador.correo,
+        cedula: centro.administrador.cedula
+    },
+    direccion: centro.direccion.provincia + ", " + centro.direccion.canton + ", " + 
+            centro.direccion.distrito + ". " + centro.direccion.senas,
+    materiales: centro.materiales 
+
+  })); 
+    response.json(datos);
+};
+
--- server/routes/centroRoutes.js
+++ server/routes/centroRoutes.js
@@ -5,4 +5,8 @@ const  centroController = require("../controllers/centroController");
 
 router.get("/", centroController.get);
 
+router.get("/:idCentro", centroController.getByIdCentro);
+router.get("/admin/:idAdmin", centroController.getByIdUser);
+
+
 module.exports = router;
