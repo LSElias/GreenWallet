8e8b512cdade2744bc02f8c337a77c6f5b596f1e
LSElias <eliasrodguer12@gmail.com>
2023-11-01 13:25:30 -0600

    Listado centros

--- client/appGreenWallet/src/app/centro/centro-index/centro-index.component.ts
+++ client/appGreenWallet/src/app/centro/centro-index/centro-index.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Subject, takeUntil } from 'rxjs';
+import { GenericService } from 'src/app/share/generic.service';
 
 @Component({
   selector: 'app-centro-index',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./centro-index.component.css']
 })
 export class CentroIndexComponent {
+  datos: any;
+  destroy$: Subject<boolean>=new Subject<boolean>();
+
+  constructor(private gService: GenericService,
+    private dialog:MatDialog, ){
+        this.listarCentros();
+    }
+
+    listarCentros(){
+      this.gService.list('centro')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: any) => {
+        console.log(data);
+        this.datos = data;
+      });
+    }
+
+    ngOnDestroy(){
+      this.destroy$.next(true);
+      this.destroy$.unsubscribe();
+    }
 
 }
--- client/appGreenWallet/src/app/centro/centro.module.ts
+++ client/appGreenWallet/src/app/centro/centro.module.ts
@@ -4,6 +4,20 @@ import { CommonModule } from '@angular/common';
 import { CentroRoutingModule } from './centro-routing.module';
 import { CentroIndexComponent } from './centro-index/centro-index.component';
 import { CentroDetalleComponent } from './centro-detalle/centro-detalle.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +27,21 @@ import { CentroDetalleComponent } from './centro-detalle/centro-detalle.componen
   ],
   imports: [
     CommonModule,
-    CentroRoutingModule
+    CentroRoutingModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDividerModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule
   ]
 })
 export class CentroModule { }
