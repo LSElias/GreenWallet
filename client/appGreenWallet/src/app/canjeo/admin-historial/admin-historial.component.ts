c0a0b4600070f71c5955fd4d4a1490f708f0f7ea
LSElias <eliasrodguer12@gmail.com>
2023-11-09 03:23:51 -0600

    admin intercambios

--- client/appGreenWallet/src/app/canjeo/admin-historial/admin-historial.component.ts
+++ client/appGreenWallet/src/app/canjeo/admin-historial/admin-historial.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subject, takeUntil } from 'rxjs';
+import { GenericService } from 'src/app/share/generic.service';
 
 @Component({
   selector: 'app-admin-historial',
@@ -6,5 +11,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-historial.component.css']
 })
 export class AdminHistorialComponent {
+  datos: any;
+  destroy$: Subject<boolean>=new Subject<boolean>();
+
+
+  @ViewChild
+  (MatPaginator) paginator!: MatPaginator;
+  @ViewChild
+  (MatSort) sort!: MatSort;
+  dataSource = new MatTableDataSource<any>();
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['fecha', 'usuario', 'accion'];
+
+
+  constructor(private gService: GenericService ){
+
+  }
+
+  ngAfterViewInit(): void {
+    this.getInfo();   
+    this.dataSource.sort = this.sort;
+  }
+
+
+    getInfo(){
+      var id= 3
+      this.gService.get('canjeo/admin', id)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response: any) => {
+        console.log(response);
+        this.datos=response;
+        this.dataSource = new MatTableDataSource(this.datos);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      });
+    }
+
+    redirect(id:any){
+      if(!Number.isNaN(id)){
+        document.location.href = `/canjeo/detalle/${id}`
+      }  
+    }
+
+
+    ngOnDestroy(){
+      this.destroy$.next(true);
+      this.destroy$.unsubscribe();
+    }
 
 }
