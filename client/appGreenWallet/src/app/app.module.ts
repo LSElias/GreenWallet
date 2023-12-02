bef4f690ed9209456c13f95495549bfcfed50fa0
Tylinn <tgonzalezb@est.utn.ac.cr>
2023-12-02 00:55:26 -0600

    Cambios

--- client/appGreenWallet/src/app/app.module.ts
+++ client/appGreenWallet/src/app/app.module.ts
@@ -12,11 +12,9 @@ import { CentroModule } from './centro/centro.module';
 import { MaterialesModule } from './materiales/materiales.module';
 import { CanjeoModule } from './canjeo/canjeo.module';
 import { ToastrModule } from 'ngx-toastr';
-<<<<<<< HEAD
 import { HttpErrorInterceptorService } from './share/http-error-interceptor.service';
-=======
 import { RecompensaModule } from './recompensa/recompensa.module';
->>>>>>> 732cbd97d0966eed1639a55e7f0d8c21397a2016
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -38,15 +36,11 @@ import { RecompensaModule } from './recompensa/recompensa.module';
 
     AppRoutingModule,
   ],
-<<<<<<< HEAD
+
   providers: [{
     provide: HTTP_INTERCEPTORS,
      useClass: HttpErrorInterceptorService, multi: true
     }],    
   bootstrap: [AppComponent]
-=======
-  providers: [],
-  bootstrap: [AppComponent],
->>>>>>> 732cbd97d0966eed1639a55e7f0d8c21397a2016
 })
 export class AppModule {}
