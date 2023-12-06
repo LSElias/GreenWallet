32abed82fab086f0e120d0d4837de0e0ae1ab33f
Tylinn <tgonzalezb@est.utn.ac.cr>
2023-12-05 20:54:21 -0600

    Reporte Admin General

--- client/appGreenWallet/src/app/reportes/reportes-routing.module.ts
+++ client/appGreenWallet/src/app/reportes/reportes-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReporteGeneralComponent } from './reporte-general/reporte-general.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'centro/reportes',
+    component: ReporteGeneralComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- client/appGreenWallet/src/app/reportes/reportes.module.ts
+++ client/appGreenWallet/src/app/reportes/reportes.module.ts
@@ -3,19 +3,34 @@ import { CommonModule } from '@angular/common';
 
 import { ReportesRoutingModule } from './reportes-routing.module';
 import { ReporteGeneralComponent } from './reporte-general/reporte-general.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-  
     ReporteGeneralComponent
   ],
   imports: [
     CommonModule,
-    ReportesRoutingModule
-  ],
-  exports: [
-    ReporteGeneralComponent
+    ReportesRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatDividerModule,
+    FormsModule, ReactiveFormsModule,
+
   ]
 })
 export class ReportesModule { }
